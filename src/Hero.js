84360f4628e745d9f29fa40b8cb0d54561a2b379
VaughnValle <vaughnmatthewvalle@gmail.com>
2024-04-29 05:22:53 -0400

    Refined Gallery and implemented ToDo

--- src/Hero.js
+++ src/Hero.js
@@ -3,17 +3,28 @@ import './global.css'
 import './gallery.css'
 import {useState} from 'react'
 import Gallery from './Gallery';
+import Todo from './Todo';
+
 const images = "./images/"
 
 const Hero = () => {
     const charles = require(images + 'charles.jpg');
-    const [shown, setShow] = useState(false);
+    const [shownGal, setGalShow] = useState(false);
+    const [shownTo, setToShow] = useState(false);
     const showGallery = () => {
-        if (shown === true){
-            setShow(false);
+        if (shownGal === true){
+            setGalShow(false);
         }
         else {
-            setShow(true);
+            setGalShow(true);
+        }
+    }
+    const showTodo = () => {
+        if (shownTo === true){
+            setToShow(false);
+        }
+        else {
+            setToShow(true);
         }
     }
     return(
@@ -24,6 +35,7 @@ const Hero = () => {
             <a href="/">Home</a> <a href="https://www.vaughnvalle.me/" target="_blank" rel="noreferrer">About</a> <a href="https://drive.google.com/file/d/17KcHZu-u0fQfqyv6D6Gdocxfv1-uOTjK/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
           </div>
         </nav>
+        {shownGal && <Gallery />}
         <section class="main">
           <div class="main-container">
             <div class="left">
@@ -32,6 +44,8 @@ const Hero = () => {
           Don't let your graduation photos be an afterthought! Capture this momentous occasion with a professional photoshoot that will leave you with stunning memories. Booking is easy, so lock in your date and get ready to celebrate your accomplishment in style.
               </p>
               <button id="gal" onClick={showGallery}> Gallery </button>
+                <br/>
+              <button id="book" onClick={showTodo}> Book Now </button>
             </div>
             <div class="right">
               <img
@@ -40,9 +54,9 @@ const Hero = () => {
                 alt="Charles"
               />
             </div>
+            {shownTo && <Todo />}
           </div>
         </section>
-        {shown && <Gallery />}
         </>
     );
 
